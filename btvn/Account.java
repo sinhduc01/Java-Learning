3ec80dd1289b8e2ec54e9132b60afb103b486340
sinhduc01 <terragm01@gmail.com>
2022-09-14 19:21:49 +0700

    bai tap account

---
+++ btvn/Account.java
@@ -0,0 +1,48 @@
+package btvn;
+
+public class Account {
+    int id;
+    String name;
+    int balance;
+
+    public Account(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
+    }
+
+    public Account(int id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    void display() {
+        System.out.println("STK:" + id + ", Ten TK:" + name + ", So du: " + balance);
+    }
+
+    void deposit(int amount) {
+        this.balance += amount;
+        System.out.println("So du trong tai khoan hien tai la" + this.balance);
+    }
+
+    void withdraw(int amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            System.out.println("So du con lai trong tai khoan:" + this.balance);
+        } else {
+            System.out.println("So du trong tai khoan khong du");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Account tk1 = new Account(92, "Hong Tam");
+        Account tk2 = new Account(232, "Sinh Duc", 1000000);
+        tk1.display();
+        tk2.display();
+        tk1.deposit(500000);
+        tk2.withdraw(2000000);
+        tk1.withdraw(100000);
+    }
+}
